d2e21d382aa2db364d9a8442d251004f314728b4
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-01-02 07:37:51 -0800

    update

--- metaBlog/src/pages/BlogForm.jsx
+++ metaBlog/src/pages/BlogForm.jsx
@@ -15,10 +15,10 @@ const BlogForm = () => {
        <form className=" max-w-[600px]" onSubmit={handleSubmit}>
          <div className="font-[600]  text-[24px] border-[#E6E6F0] mb-[40px] border-b-[1px] pb-[20px] text-[#32264D] "> Blog Form</div> 
           <label className=" text-[14px] text-[#4E4958] block  "  htmlFor="title">Title</label>
-          <input className="border p-[12px] sm:p-[20px] rounded-[8px] outline-none mt-[12px] w-[270px] sm:w-[320px] md:w-[400px] lg:w-[600px] " id="title" type="text" />
+          <input className="border p-[12px] text-black sm:p-[20px] rounded-[8px] outline-none mt-[12px] w-[270px] sm:w-[320px] md:w-[400px] lg:w-[600px] " id="title" type="text" />
       
           <label className=" text-[14px] text-[#4E4958] block mt-[40px]  "  htmlFor="description">Description</label>
-          <textarea className="border p-[12px] sm:p-[20px] rounded-[8px] outline-none mt-[12px] w-[270px] sm:w-[320px] md:w-[400px] lg:w-[600px] resize-none " rows={4} id="description" type="text" />
+          <textarea className="border text-black p-[12px] sm:p-[20px] rounded-[8px] outline-none mt-[12px] w-[270px] sm:w-[320px] md:w-[400px] lg:w-[600px] resize-none " rows={4} id="description" type="text" />
           <button type="submit" className="bg-[#4B6BFB] text-[16px] font-[600px] text-white w-full text-center rounded-[8px] py-[15px] mt-[40px] mb-[45px] active:opacity-[0.8] "  >Submit</button>
         
        </form>
